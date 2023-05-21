3321b87ee62d5da36994f4a199abe777a91c38e4
Gustavo Prevelate <guprevelate2022@gmail.com>
2023-05-20 22:42:25 -0300

    Réplica do projeto, Responsividade main do Index.html

---
+++ js/router.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const routes = {
+    '/': '/pages/inicio.html',
+    '/projetos': '/pages/projetos.html',
+    '/empresas': '/pages/empresas.html',
+    '/sobre_nos': '/pages/sobre_nos.html',
+    '/pontos_coleta': '/pages/pontos_coleta.html',
+    '/faca_parte': '/pages/faca_parte.html'
+}
+
+const route = async () => {
+    window.event.preventDefault()
+    window.history.pushState({}, "", window.event.target.href)
+
+    const path = window.location.pathname
+    const route = routes[path]
+    const response = await fetch(route)
+    const html = await response.text()
+
+    document.getElementById('root').innerHTML = html
+
+    console.log(html)
+
+
+}
+
+window.route = route
